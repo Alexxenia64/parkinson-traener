b4cc6427aded3cd1af94a4468dbd0505ecbcc9b9
Alexxenia64 <petersolgaard@hotmail.com>
2026-05-28 09:31:43 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 body \{\
+    font-family: Arial, sans-serif;\
+    background: #e8f1ff;\
+    margin: 0;\
+    padding: 0;\
+    text-align: center;\
+\}\
+\
+.screen \{\
+    display: none;\
+    padding: 20px;\
+\}\
+\
+.screen.active \{\
+    display: block;\
+\}\
+\
+button \{\
+    display: block;\
+    width: 80%;\
+    margin: 15px auto;\
+    padding: 20px;\
+    font-size: 22px;\
+    border-radius: 12px;\
+    border: none;\
+    background: #1e6bff;\
+    color: white;\
+\}\
+\
+button:hover \{\
+    background: #0047cc;\
+\}\
+\
+h1 \{\
+    color: #003a80;\
+\}\
+\
+input \{\
+    width: 60%;\
+    padding: 10px;\
+    font-size: 20px;\
+    margin-bottom: 20px;\
+\}}
